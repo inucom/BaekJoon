1697304b27923df81eddd80f3d2f6c6fe8fb170c
Dirty-c-o-d-e <113766889+Dirty-c-o-d-e@users.noreply.github.com>
2022-09-20 15:45:29 +0900

    [Silver II] Title: 골드바흐의 추측, Time: 212 ms, Memory: 2060 KB -BaekjoonHub

--- 백준/Silver/9020. 골드바흐의 추측/골드바흐의 추측.cc
+++ 백준/Silver/9020. 골드바흐의 추측/골드바흐의 추측.cc
@@ -9,16 +9,15 @@ int main() {
 	for (int j = 2; j < 10001; j++) {
 		ans[j] = j;
 	}
+	for (int j = 2; j <= 100; j++) {
+		if (ans[j] == 0)
+			continue;
+		for (int k = 2 * j; k <= 10000; k += j) {
+			ans[k] = 0;
+		}
+	}
 	for (int i = 0; i < t; i++) {
 		cin >> prime;
-
-		for (int j = 2; j <= prime; j++) {
-			if (ans[j] == 0)
-				continue;
-			for (int k = 2 * j; k <= prime; k += j) {
-				ans[k] = 0;
-			}
-		}
 		if (prime % 2 == 0) {
 			for (int j = 0; j <= (prime / 2); j++) {
 				if ((ans[prime / 2 - j] != 0) && (ans[prime / 2 + j] != 0)) {
@@ -35,7 +34,6 @@ int main() {
 				}
 			}
 		}
-
 		/*as1 = -1;
 		for (int j = 2; j <= prime; j++) {
 			for (int k = j; k<=prime;k++){
