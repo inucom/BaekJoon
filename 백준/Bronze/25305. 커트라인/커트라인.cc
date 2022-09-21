d3f238a04f012e6f2de485175799e011205245b7
Dirty-c-o-d-e <113766889+Dirty-c-o-d-e@users.noreply.github.com>
2022-09-21 20:46:27 +0900

    [Bronze II] Title: 커트라인, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/25305. 커트라인/커트라인.cc
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+int score[1000];
+int main() {
+	int n, k;
+	cin >> n >> k;
+	for (int i = 0; i < n; i++) {
+		cin >> score[i];
+	}
+	sort(score, score + n);
+	cout << score[n-k];
+}
