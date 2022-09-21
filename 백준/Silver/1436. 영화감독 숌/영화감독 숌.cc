4262d1570bf9aee5a30c8a3114e3c137ab5b0e23
Dirty-c-o-d-e <113766889+Dirty-c-o-d-e@users.noreply.github.com>
2022-09-21 16:47:46 +0900

    [Silver V] Title: 영화감독 숌, Time: 28 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/1436. 영화감독 숌/영화감독 숌.cc
+++ 백준/Silver/1436. 영화감독 숌/영화감독 숌.cc
@@ -1,5 +1,5 @@
 #include <iostream>
-#include <string>
+//#include <string>
 using namespace std;
 int main() {
 	ios::sync_with_stdio(false);
@@ -7,19 +7,29 @@ int main() {
 	int n, cnt = 1;
 	cin >> n;
 	int ans = 666;
-	int i = 1;
-	string str;
+	int temp;
+	//int i = 1;
+	//string str;
 	while (n != cnt) {
-		ans = ans + 1;
-		str = to_string(ans);
+		ans++;
+		temp = ans;
+		//str = to_string(ans);
 		//cout << str<<"  ";
-		for (int j = 0; j < str.length()-2; j++) {
+		while (temp != 0) {
+			if (temp % 1000 == 666) {
+				cnt++;
+				break;
+			}
+			else
+				temp /= 10;
+		}
+		/*for (int j = 0; j < str.length()-2; j++) {
 			if (str[j] == '6' && str[j + 1] == '6' && str[j + 2] == '6') {
 				cnt++;
 				break;
 			}
 			
-		}
+		}*/
 		//cout << cnt << " ";
 	}
 	cout << ans;
