48a45cd3cb36553be4c506a2a8ccfc165d38a92f
Dirty-c-o-d-e <113766889+Dirty-c-o-d-e@users.noreply.github.com>
2022-09-23 00:45:13 +0900

    [Silver II] Title: 좌표 압축, Time: 988 ms, Memory: 12408 KB -BaekjoonHub

---
+++ 백준/Silver/18870. 좌표 압축/좌표 압축.cc
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+#include <vector>
+int main() {
+	int n;
+	cin >> n;
+	vector<int> crd, coo;
+	int a;
+	for (int i = 0; i < n; i++) {
+		cin >> a;
+		coo.push_back(a);
+		crd.push_back(a);
+	}
+	sort(crd.begin(), crd.end());
+	crd.erase(unique(crd.begin(), crd.end()), crd.end());
+
+	for (int i = 0; i < n; i++) {
+		cout<<lower_bound(crd.begin(),crd.end(),coo[i]) - crd.begin() << " ";
+	}
+}
