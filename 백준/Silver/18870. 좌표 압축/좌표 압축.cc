b1a19f6fc9ebf073cce73d9bbd15d48a911b65b0
Dirty-c-o-d-e <113766889+Dirty-c-o-d-e@users.noreply.github.com>
2022-09-23 00:48:32 +0900

    [Silver II] Title: 좌표 압축, Time: 472 ms, Memory: 12524 KB -BaekjoonHub

--- 백준/Silver/18870. 좌표 압축/좌표 압축.cc
+++ 백준/Silver/18870. 좌표 압축/좌표 압축.cc
@@ -1,8 +1,12 @@
+
 #include <iostream>
 #include <algorithm>
 using namespace std;
 #include <vector>
 int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
 	int n;
 	cin >> n;
 	vector<int> crd, coo;
