6d70a0618987e79fd8d329a7610d364aab686734
Dirty-c-o-d-e <113766889+Dirty-c-o-d-e@users.noreply.github.com>
2022-09-24 13:37:55 +0900

    [Silver IV] Title: 요세푸스 문제, Time: 76 ms, Memory: 2152 KB -BaekjoonHub

---
+++ 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.cc
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <queue>
+using namespace std;
+queue<int> jose;
+int main() {
+    	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	int n, k;
+	cin >> n >> k;
+	for (int i = 1; i <= n; i++) {
+		jose.push(i);
+	}
+	int a;
+	cout << "<";
+	while (jose.empty() != 1) {
+		for (int i = 0; i < k-1; i++) {
+			a = jose.front();
+			jose.pop();
+			jose.push(a);
+		}
+		a = jose.front();
+		jose.pop();
+		cout << a;
+		if (jose.empty() != 1) { cout << ", "; }
+	}
+	cout << ">";
+}
