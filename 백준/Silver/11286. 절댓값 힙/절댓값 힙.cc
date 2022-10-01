8b3615b1760ba25e6cc3abd0ef85df7e20c55218
Dirty-c-o-d-e <113766889+Dirty-c-o-d-e@users.noreply.github.com>
2022-10-01 14:04:59 +0900

    [Silver I] Title: 절댓값 힙, Time: 16 ms, Memory: 2916 KB -BaekjoonHub

--- 백준/Silver/11286. 절댓값 힙/절댓값 힙.cc
+++ 백준/Silver/11286. 절댓값 힙/절댓값 힙.cc
@@ -13,6 +13,9 @@ struct cmp {
 };
 
 int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
 	int n;
 	cin >> n;
 	priority_queue<pair<int,int>,vector<pair<int,int>>, cmp>que;
@@ -30,5 +33,4 @@ int main() {
 				cout << 0<<"\n";
 		}
 	}
-
 }
