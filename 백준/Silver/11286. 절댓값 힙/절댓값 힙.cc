65e665cb021a69e178cef80e4194e434b691c9a3
Dirty-c-o-d-e <113766889+Dirty-c-o-d-e@users.noreply.github.com>
2022-10-01 14:04:30 +0900

    [Silver I] Title: 절댓값 힙, Time: 900 ms, Memory: 2804 KB -BaekjoonHub

---
+++ 백준/Silver/11286. 절댓값 힙/절댓값 힙.cc
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <queue>
+#include <cmath>
+using namespace std;
+
+struct cmp {
+	bool operator()(pair<int, int>a, pair<int, int>b) {
+		if (a.first == b.first)
+			return a.second > b.second;
+		else
+			return a.first > b.first;
+	}
+};
+
+int main() {
+	int n;
+	cin >> n;
+	priority_queue<pair<int,int>,vector<pair<int,int>>, cmp>que;
+	int input;
+	for (int i = 0; i < n; i++) {
+		cin >> input;		if (input != 0) {
+			que.emplace(abs(input),input);
+		}
+		else {
+			if (que.empty() != 1) {
+				cout << que.top().second<< "\n";
+				que.pop();
+			}
+			else
+				cout << 0<<"\n";
+		}
+	}
+
+}
