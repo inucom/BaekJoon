87202a2724a6cc9c1835241a5b08fa0dca9da29a
Dirty-c-o-d-e <113766889+Dirty-c-o-d-e@users.noreply.github.com>
2022-10-08 15:48:17 +0900

    [Silver III] Title: 1, 2, 3 더하기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.cc
+++ 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.cc
@@ -8,11 +8,14 @@ int main() {
 	dp[2] = 2;
 	dp[3] = 4;
 	dp[4] = 7;
+	dp[5] = 13;
+	dp[6] = 24;
+	dp[7] = 44;
+	dp[8] = 81;
+	dp[9] = 149;
+	dp[10] = 274;
 	for (int a = 0; a < t; a++) {
 		cin >> n;
-		for (int i = 4; i <= n; i++) {
-			dp[i] = dp[i - 1] + dp[i - 2] + dp[i - 3];
-		}
 		cout << dp[n] << '\n';
 	}
 }
