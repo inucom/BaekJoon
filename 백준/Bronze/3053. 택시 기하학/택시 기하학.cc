c67eefcb10e81a789e39343d03aa5d5d78305496
Dirty-c-o-d-e <113766889+Dirty-c-o-d-e@users.noreply.github.com>
2022-11-05 17:27:53 +0900

    [Bronze III] Title: 택시 기하학, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/3053. 택시 기하학/택시 기하학.cc
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main() {
+	int r;
+	cin >> r;
+	const long double pi = 3.1415926535897932;
+	cout << fixed;
+	cout.precision(6);
+	cout << r* r * pi<<'\n';
+	cout << r * r * 2.0;
+
+}
