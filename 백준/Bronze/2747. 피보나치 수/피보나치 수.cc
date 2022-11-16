816349fc099fb74985782acc1f12cbb3a1a95dad
방승호 <113766889+inucom@users.noreply.github.com>
2022-11-16 14:09:21 +0900

    [Bronze II] Title: 피보나치 수, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2747. 피보나치 수/피보나치 수.cc
@@ -0,0 +1,13 @@
+#include <iostream>
+long long dp[46];
+int main() {
+	dp[0] = 0;
+	dp[1] = 1;
+	dp[2] = 1;
+	int n;
+	std::cin >> n;
+	for (int i = 3; i <= n; i++) {
+		dp[i] = dp[i - 1] + dp[i - 2];
+	}
+	std::cout << dp[n];
+}
