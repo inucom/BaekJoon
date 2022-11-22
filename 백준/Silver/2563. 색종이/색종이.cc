d257ab49485a7f0f2c3d8598cd32ff948f12b244
방승호 <113766889+inucom@users.noreply.github.com>
2022-11-23 01:27:17 +0900

    [Silver V] Title: 색종이, Time: 0 ms, Memory: 2032 KB -BaekjoonHub

--- 백준/Silver/2563. 색종이/색종이.cc
+++ 백준/Silver/2563. 색종이/색종이.cc
@@ -8,8 +8,8 @@ int main() {
 	int ans = 0;
 	for (int i = 0; i < n; i++) {
 		cin >> x >> y;
-		for (int j = x-1; j < x+9; j++) {
-			for (int k = y-1; k < y+9; k++) {
+		for (int j = x; j < x+10; j++) {
+			for (int k = y; k < y+10; k++) {
 				if (checkBoard[j][k] == false) {
 					checkBoard[j][k] = true;
 					ans++;
