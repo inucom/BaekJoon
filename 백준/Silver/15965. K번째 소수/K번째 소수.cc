809790dcd02d4fb879105311f8a5d24e181d7ca9
방승호 <113766889+inucom@users.noreply.github.com>
2022-12-01 17:31:28 +0900

    [Silver II] Title: K번째 소수, Time: 72 ms, Memory: 9344 KB -BaekjoonHub

---
+++ 백준/Silver/15965. K번째 소수/K번째 소수.cc
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+bool arr[7500000] = { 1,1,0,0, };
+int main() {
+	int k;
+	cin >> k;
+	for (int i = 2; i <7500000 ; i++) {
+		if (arr[i]) {
+			continue;
+		}
+		for (int j = 2 * i; j < 7500000; j += i) {
+			arr[j] = 1;
+		}
+	}
+	int index = 0, cnt = 0;
+	while (cnt < k) {
+		if (!arr[index]) {
+			cnt++;
+			//cout << index << '\n';
+		}
+		index++;
+	}
+	cout << index-1;
+}
