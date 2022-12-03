7917ec641301d5b8d88e00fb07afed214bbb40b4
방승호 <113766889+inucom@users.noreply.github.com>
2022-12-03 20:42:41 +0900

    [Bronze II] Title: 소수 단어, Time: 0 ms, Memory: 2036 KB -BaekjoonHub

---
+++ 백준/Bronze/2153. 소수 단어/소수 단어.cc
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <string>
+using namespace std;
+bool arr[11000] = { 1,0,0,0, };
+int main() {
+	string input;
+	cin >> input;
+	int n = input.length();
+	int sum = 0;
+	for(int i = 0; i< n ; i++){ 
+		if (input[i] >= 'a' && input[i] <= 'z')
+			sum += input[i] - 'a' + 1;
+		else if (input[i] >= 'A' && input[i] <= 'Z')
+			sum += input[i] - 'A' + 27;
+	}
+	for (int i = 2; i <= 4000; i++) {
+		if (arr[i])continue;
+		for (int j = i * 2; j < 11000; j += i) {
+			arr[j] = 1;
+		}
+	}
+	if (!arr[sum]) {
+		cout << "It is a prime word.";
+	}
+	else if(arr[sum])
+		cout << "It is not a prime word.";
+}
