6810e3310c7922e42ff7cec4914627977feb533e
방승호 <113766889+inucom@users.noreply.github.com>
2022-12-13 23:13:36 +0900

    [Silver III] Title: N과 M (3), Time: 520 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/15651. N과 M （3）/N과 M （3）.cc
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+int n, m;
+int arr[8];
+void dfs(int cnt) {
+	if (cnt == m) {
+		for (int i = 0; i < m; i++) {
+			cout << arr[i] << ' ';
+		}
+		cout << "\n";
+		return;
+	}
+
+	for (int i = 1; i <= n; i++) {
+		arr[cnt] = i;
+		dfs(cnt + 1);
+	}
+
+}
+
+int main() {
+	cin >> n >> m;
+	dfs(0);
+}
