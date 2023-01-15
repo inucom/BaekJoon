7f7311bb690de6532cb36ad9a5fd89e56fb07343
방승호 <113766889+inucom@users.noreply.github.com>
2023-01-15 12:49:36 +0900

    [Bronze V] Title: 와이버스 부릉부릉, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/14645. 와이버스 부릉부릉/와이버스 부릉부릉.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int a, b;
+	cin >> a >> b;
+	int c, d;
+	for (int i = 0; i < a; i++) {
+		for (int j = 0; j < b; j++) {
+			cin >> c;
+		}
+	}
+	cout << "비와이";
+}
