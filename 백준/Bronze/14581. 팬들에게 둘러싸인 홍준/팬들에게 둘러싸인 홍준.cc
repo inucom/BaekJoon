296dbdf4aeb7e8996d6beff9f9bdeefd27392ad4
방승호 <113766889+inucom@users.noreply.github.com>
2023-01-16 10:21:06 +0900

    [Bronze V] Title: 팬들에게 둘러싸인 홍준, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/14581. 팬들에게 둘러싸인 홍준/팬들에게 둘러싸인 홍준.cc
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+	string str;
+	cin >> str;
+	cout << ":fan::fan::fan:\n";
+	cout << ":fan::" << str << "::fan:\n";
+	cout << ":fan::fan::fan:";
+}
