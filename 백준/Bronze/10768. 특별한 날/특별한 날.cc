c486e3a72a4a98140371edaa55e7a32c342b7e38
방승호 <113766889+inucom@users.noreply.github.com>
2023-02-06 22:41:33 +0900

    [Bronze IV] Title: 특별한 날, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10768. 특별한 날/특별한 날.cc
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int a, b;
+	cin >> a >> b;
+	if (a > 2) {
+		cout << "After";
+	}
+	else if (a == 2 && b > 18) {
+		cout << "After";
+	}
+	else if (a == 2 && b == 18) {
+		cout << "Special";
+	}
+	else
+		cout << "Before";
+}
