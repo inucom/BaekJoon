77b7dc8827f43ff901ddbcbeb31ef929cbda407a
방승호 <113766889+inucom@users.noreply.github.com>
2023-02-28 23:01:59 +0900

    [Bronze V] Title: 조별과제를 하려는데 조장이 사라졌다, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/15727. 조별과제를 하려는데 조장이 사라졌다/조별과제를 하려는데 조장이 사라졌다.cc
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main() {
+    int L;
+    cin >> L;
+
+    int t = ceil((double)L / 5);
+    cout << t << endl;
+
+    return 0;
+}
