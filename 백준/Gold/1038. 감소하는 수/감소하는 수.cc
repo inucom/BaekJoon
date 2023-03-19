f611ce2af308ef8dc44659ecd96ea9569cc018bf
방승호 <113766889+inucom@users.noreply.github.com>
2023-03-19 16:59:52 +0900

    [Gold V] Title: 감소하는 수, Time: 0 ms, Memory: 9832 KB -BaekjoonHub

---
+++ 백준/Gold/1038. 감소하는 수/감소하는 수.cc
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <queue>
+using namespace std;
+long long dp[1000001];
+
+int main() {
+	int n;
+	cin >> n;
+	queue<long long>add;
+	for (int i = 1; i < 10; i++) {
+		dp[i] = i;
+		add.push(i);
+	}
+	int index = 10;
+	while (index <= n && !add.empty()) {
+		long long start = add.front();
+		add.pop();
+		int plus = start % 10;
+		for (int i = 0; i < plus; i++) {
+			long long result = start * 10 + i;
+			dp[index++] = result;
+			add.push(result);
+		}
+	}
+		//for (int i = 0; i <= n; i++) {
+		//	cout << dp[i] << "||";
+		//}
+	if (n > 1022) {
+		cout << -1;
+	}
+	else {
+		cout << dp[n];
+	}
+}
