a9e55d474120e5fe44a4839c64bb36eed1003c13
방승호 <113766889+inucom@users.noreply.github.com>
2023-03-20 03:50:44 +0900

    [Gold II] Title: 버블 소트, Time: 96 ms, Memory: 8296 KB -BaekjoonHub

--- 백준/Gold/1377. 버블 소트/버블 소트.cc
+++ 백준/Gold/1377. 버블 소트/버블 소트.cc
@@ -1,41 +1,26 @@
 #include <iostream>
 #include <algorithm>
 #include <vector>
-#include <cmath>
 using namespace std;
 
 
 int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
 	int N;
 	cin >> N;
 	vector<pair<int, int>> A;
-	vector<pair<int, int>> B;
 	int input;
 	for (int i = 0; i < N; i++) {
 		cin >> input;
 		A.push_back({ input,i });
-		B.push_back({ input,i });
 	}
-
 	sort(A.begin(),A.end());
 	int cpr = 0, ans = 0;
 	for (int i = 0; i < N; i++) {
-		cpr = A[i].second - B[i].second;
-		//cout <<B[i].first << "  B:" << B[i].second << "  A:" << A[i].second << "  cpr:" << cpr << "\n";
-		if (ans < cpr)
-			ans = cpr;
+		ans = max(A[i].second - i+1,ans);
 	}
-	cout << ans + 1;
-	//int ans = 0
-	//for (int i = 0; i < N; i++) {
-	//	
-	//	if (ans < cpr);
-	//	ans = cpr;
-	//}
-	//if (ans == 0) {
-	//	cout << 1;
-	//}
-	//else {
-	//	cout << cpr-1;
-	//}
+	cout << ans;
 }
