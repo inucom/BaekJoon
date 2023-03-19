2f38ed17ad22f10d426b72f58a6b8779ea4ed3c2
방승호 <113766889+inucom@users.noreply.github.com>
2023-03-20 03:56:37 +0900

    [Gold II] Title: 버블 소트, Time: 88 ms, Memory: 5928 KB -BaekjoonHub

--- 백준/Gold/1377. 버블 소트/버블 소트.cc
+++ 백준/Gold/1377. 버블 소트/버블 소트.cc
@@ -1,8 +1,7 @@
 #include <iostream>
 #include <algorithm>
-#include <vector>
 using namespace std;
-
+pair<int, int> A[500001];
 
 int main() {
 	ios_base::sync_with_stdio(false);
@@ -11,13 +10,13 @@ int main() {
 
 	int N;
 	cin >> N;
-	vector<pair<int, int>> A;
+
 	int input;
 	for (int i = 0; i < N; i++) {
 		cin >> input;
-		A.push_back({ input,i });
+		A[i] = { input,i };
 	}
-	sort(A.begin(),A.end());
+	sort(A,A+N);
 	int cpr = 0, ans = 0;
 	for (int i = 0; i < N; i++) {
 		ans = max(A[i].second - i+1,ans);
