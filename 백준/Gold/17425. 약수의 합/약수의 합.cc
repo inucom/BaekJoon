ae1be190267eccce00d53c74aa28a4fb54b73fcf
방승호 <113766889+inucom@users.noreply.github.com>
2023-03-23 16:57:54 +0900

    [Gold IV] Title: 약수의 합, Time: 100 ms, Memory: 9832 KB -BaekjoonHub

--- 백준/Gold/17425. 약수의 합/약수의 합.cc
+++ 백준/Gold/17425. 약수의 합/약수의 합.cc
@@ -6,13 +6,11 @@ int main() {
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL);
 	cout.tie(NULL);
-	int t;
+	int t, n;
 	cin >> t;
-	int n;
-	arr[1] = 1;
-
-	for (int i = 2; i <= 1000000; i++) {
-		arr[i] += 1 + arr[i - 1];
+	arr[0] = 0;
+	for (int i = 1; i <= 1000000; i++) {
+		arr[i] += arr[i - 1];
 		for (int j = i ; j <= 1000000; j += i) {
 			arr[j] += i;
 		}
