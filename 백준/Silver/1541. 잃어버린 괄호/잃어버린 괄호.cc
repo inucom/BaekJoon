df18374a418fa5f69e4fc9d007120eb9ca33466e
방승호 <113766889+inucom@users.noreply.github.com>
2023-03-31 23:58:07 +0900

    [Silver II] Title: 잃어버린 괄호, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/1541. 잃어버린 괄호/잃어버린 괄호.cc
+++ 백준/Silver/1541. 잃어버린 괄호/잃어버린 괄호.cc
@@ -4,44 +4,30 @@
 
 using namespace std;
 
-int arr[51];
-
 int main() {
 	string str;
 	cin >> str;
-	int index = 0, num = 0, minus = 0, plus = 0, index_op = 0;
-
+	int index = 0, num = 0;
+	int ans = 0;
+	bool check = false;
+	str += '+';
 	for (int i = 0; i < str.length(); i++) {
-		if (str[i] != '+' && str[i] != '-') {
-			num = num * 10 + (str[i] -'0');
-		}
-		if(str[i] == '+' || str[i] == '-') {
-			arr[index++] = num;
-			num = 0;
-			if (str[i] == '+') {
-				arr[index++] = -1;
+		if (str[i] == '+' || str[i] == '-') {
+			if (check == false) {
+				ans += num;
+				num = 0;
 			}
-			if (str[i] == '-') {
-				arr[index++] = -2;
+			else {
+				ans -= num;
+				num = 0;
 			}
 		}
-	}
-	arr[index++] = num;
-
-	int ans = arr[0];
-	bool check_oper = false;
-	for (int i = 1; i < index; i++) {
-		if (arr[i] == -2) {
-			check_oper = true;
-			continue;
+		else{
+			num = num * 10 + (str[i] - '0');
 		}
-		if (check_oper == true && arr[i] >=0) {
-			ans -= arr[i];
+		if (str[i] == '-') {
+			check = true;
 		}
-		else if (arr[i] >=0)
-			ans += arr[i];
-		//cout << ans << "\n";
-		
 	}
 	cout << ans;
 }
