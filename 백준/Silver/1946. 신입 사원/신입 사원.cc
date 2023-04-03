1894b2e7738f0f946647fed5a39a708dae6ea8f3
방승호 <113766889+inucom@users.noreply.github.com>
2023-04-03 17:04:24 +0900

    [Silver I] Title: 신입 사원, Time: 1588 ms, Memory: 2800 KB -BaekjoonHub

---
+++ 백준/Silver/1946. 신입 사원/신입 사원.cc
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+pair<int, int> arr[100001];
+
+int main() {
+	int t;
+	cin >> t;
+	int n;
+	for (int i = 0; i < t; i++) {
+		cin >> n;
+		for (int j = 0; j < n; j++) {
+			int a, b;
+			cin >> a >> b;
+			arr[j] = { a,b };
+		}
+		sort(arr, arr + n);
+		int min = 100001, ans = 0;
+		for (int j = 0; j < n; j++) {
+			//cout << arr[j].second << " second " << min << " min \n";
+			if (arr[j].second < min) {
+				min = arr[j].second;
+				ans++;
+			}
+		}
+		cout << ans << "\n";
+	}
+}
