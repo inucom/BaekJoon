6bbd5ad00d92bc557721da9b2a650b850ace9e33
방승호 <113766889+inucom@users.noreply.github.com>
2023-04-04 21:13:28 +0900

    [Gold II] Title: 보석 도둑, Time: 460 ms, Memory: 8624 KB -BaekjoonHub

---
+++ 백준/Gold/1202. 보석 도둑/보석 도둑.cc
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <queue>
+#include <algorithm>
+
+using namespace std;
+
+pair<int, int> jewel[300001];
+int bag[300001];
+
+
+int main() {
+	int n, k;
+	cin >> n >> k;
+	int m, v;
+	for (int i = 0; i < n; i++) {
+		cin >> m >> v;
+		jewel[i] = { m,v };
+	}
+	sort(jewel, jewel + n);
+	for (int i = 0; i < k; i++) {
+		cin >> bag[i];
+	}
+	sort(bag, bag + k);
+	priority_queue <int> pq;
+	int index = 0;
+	long long ans = 0;
+	for (int i = 0; i < k; i++) {
+		while (jewel[index].first <= bag[i]) {
+			pq.push(jewel[index].second);
+			index++;
+		}
+		if (!pq.empty()) {
+			ans += pq.top();
+			pq.pop();
+		}
+
+	}
+	cout << ans;
+}
