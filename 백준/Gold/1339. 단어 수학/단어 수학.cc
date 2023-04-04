517fb31c667cfb3a2d3464edfe586c19fe006156
방승호 <113766889+inucom@users.noreply.github.com>
2023-04-04 15:43:44 +0900

    [Gold IV] Title: 단어 수학, Time: 0 ms, Memory: 2212 KB -BaekjoonHub

--- 백준/Gold/1339. 단어 수학/단어 수학.cc
+++ 백준/Gold/1339. 단어 수학/단어 수학.cc
@@ -7,31 +7,16 @@ using namespace std;
 string str[11];
 int arr[26];
 
-//bool comp(string a, string b) {
-//	if (a.length() == b.length())
-//		return a.length() > b.length();
-//	else
-//		return a.length() > b.length();
-//}
-
-
-
 int main() {
 	int n;
 	cin >> n;
-		for (int i = 0; i < n; i++) {
-		cin >> str[i];
-	}
-	//sort(str, str + n, comp);
 	for (int i = 0; i < n; i++) {
+		cin >> str[i];
 		for (int j = 0; j < str[i].length(); j++) {
 			arr[str[i][j] - 'A'] += pow(10, str[i].length() - 1 - j);
 		}
 	}
 	sort(arr, arr + 26, greater<int>());
-	//for (int i = 0; i < 26; i++) {
-	//	cout << arr[i] << " ";
-	//}
 	int index = 0;
 	int ans = 0;
 	while (arr[index] != 0) {
