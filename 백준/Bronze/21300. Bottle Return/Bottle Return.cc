e7b0b0e3567952fe7891442861e4aece27540ee3
방승호 <113766889+inucom@users.noreply.github.com>
2023-04-27 21:40:33 +0900

    [Bronze V] Title: Bottle Return, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/21300. Bottle Return/Bottle Return.cc
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main(void) {
+    int tmp;
+    int sum = 0;
+    for (int i = 0; i < 6; i++) {
+        cin >> tmp;
+        sum += tmp;
+    }
+    cout << sum*5;
+}
