39ba8d35c27b528fa8bf823f5ea52607157fb768
방승호 <113766889+inucom@users.noreply.github.com>
2023-05-07 23:50:27 +0900

    [Bronze V] Title: 팩토리얼 2, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/27433. 팩토리얼 2/팩토리얼 2.cc
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main() {
+    int input;
+    cin >> input;
+    if (input == 0) { cout << 1; return 0; }
+    long long tmp = 1;
+    for (int i = 1; i <= input; ++i) tmp *= i;
+    cout<< tmp;
+}
