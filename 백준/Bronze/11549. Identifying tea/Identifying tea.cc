1861466c5e5565d86ab2a04cf9f4acda23a7fd5d
방승호 <113766889+inucom@users.noreply.github.com>
2023-05-30 22:41:26 +0900

    [Bronze IV] Title: Identifying tea, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/11549. Identifying tea/Identifying tea.cc
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int t, tmp, ans = 0;
+    cin>>t;
+    for(int i = 0; i < 5; i++){
+        cin>>tmp;
+        if(t == tmp)
+            ans += 1;
+    }
+    cout<<ans;
+}
