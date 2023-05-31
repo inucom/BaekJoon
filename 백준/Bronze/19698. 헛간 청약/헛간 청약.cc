1ca5b873af9531704e1df9bda0094a7a4c2cc105
방승호 <113766889+inucom@users.noreply.github.com>
2023-05-31 21:03:29 +0900

    [Bronze IV] Title: 헛간 청약, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/19698. 헛간 청약/헛간 청약.cc
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int n, w, h, l;
+    cin >> n >> w >> h >> l;
+    int ans = (w / l) * (h / l);
+    n < ans ? cout << n : cout << ans;
+}
